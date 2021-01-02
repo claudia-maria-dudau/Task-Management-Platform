456f768219e043e90f753f4e9d84e0477f4efebc
Costrun Larisa-Bianca <41392462+Larisuk@users.noreply.github.com>
2021-01-03 01:18:02 +0200

    ..

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -20,11 +20,12 @@ namespace Task_Management_Platform
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.bundle.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
